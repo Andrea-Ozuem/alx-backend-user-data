5fbde3c4af02c6ff86120969c145151deb2c8778
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-11-19 22:28:57 +0100

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
 import uuid
+from typing import Optional
 
 
 class Auth:
@@ -40,6 +41,26 @@ class Auth:
         u_id = _generate_uuid()
         self._db.update_user(user.id, session_id=u_id)
         return u_id
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        '''takes a single session_id string argument and returns the
+        corresponding User or None'''
+        if session_id is None:
+            return
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return
+
+    def destroy_session(self, user_id: str) -> None:
+        '''destroys a session'''
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return
+        self._db.update_user(user.id, session_id=None)
+        return
 
 
 def _hash_password(password: str) -> bytes:
