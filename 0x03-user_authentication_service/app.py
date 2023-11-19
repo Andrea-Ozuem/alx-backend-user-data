523c52f62dde33b3e79362218278ebfe69f78751
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-11-19 12:23:31 +0100

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,10 @@
 'Flask app module'
 
 from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,19 @@ app = Flask(__name__)
 def index() -> str:
     '''return a JSON payload of the form'''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    '''implements the POST /users route'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email and password:
+        try:
+            AUTH.register_user(email, password)
+            return jsonify({"email": email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
