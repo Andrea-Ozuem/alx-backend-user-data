e2c05eea7543c98fb4ba8d2a77d9201b703ab761
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-11-19 10:09:06 +0100

    fix: Remove unnecessary attr

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 'Set up Classes'
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    'Hanfles user lass for authentication'
+    'Handles user class for authentication'
     __tablename__ = 'users'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
